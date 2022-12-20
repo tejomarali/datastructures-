f7280719e4ed40707b8c1267e7f856816b58f68f
tejomarali <121115395+tejomarali@users.noreply.github.com>
2022-12-20 21:24:55 +0530

    Update linear and binary search.py

--- linear and binary search.py
+++ linear and binary search.py
@@ -16,3 +16,30 @@ print(lst1)
  
 ele = int(input(''))
 linear_search(lst1,ele)
+
+binary search:
+    
+def binary_search(arr,x,low,high):
+  if (low<=high):
+    mid = (low+high)//2
+    if(arr[mid] == x):
+      return mid
+
+    elif arr[mid] > x:
+      return binary_search(arr,x,low,mid-1)
+    else:
+      return binary_search(arr,x,mid+1,high)
+  else:
+    return -1 
+
+str_input= input( )
+list1 = [int(x) for x in str_input.split()] 
+y = int(input())
+
+res = binary_search(list1,y,0,len(list1)-1)
+if(res!=-1):
+  print("",str(res))
+else:
+  print(-1)         
+
+    
