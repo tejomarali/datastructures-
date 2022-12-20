caa59aee318858d7389cb9a7f981393b5792ccb3
tejomarali <121115395+tejomarali@users.noreply.github.com>
2022-12-20 21:38:34 +0530

    Update mergesort.py

--- mergesort.py
+++ mergesort.py
@@ -1,35 +1,42 @@
 " Merge sorting code:"
 
-def mergesort(array):
-  if len(array)>1:
-    r=len(array)//2
-    L=array[:r]
-    M=array[r:]
-    mergesort(L)
-    mergesort(M)
-    i=j=k=0
-    while i<len(L) and j<len(M):
-      if L[i]<M[j]:
-        array[k]=L[i]
-        i+=1
-      else:
-        array[k]=M[j]
-        j+=1
-      k+=1
-    while i<len(L):
-      array[k]=L[i]
-      i+=1
-      k+=1
-      while j<len(M):
-        array[k]=M[j]
-        j+=1
-        k+=1
-def printList(array):
-  for i in range (len(array)):
-    print(array[i],end=" ")
-  print()
+def mergeSort(myList):
+    if len(myList) > 1:
+        mid = len(myList) // 2
+        left = myList[:mid]
+        right = myList[mid:]
 
-if __name__ == '__main_':
-  array=[2,17,11,4,1]
-  mergesort(array)
-  print("sorted array is :",array)
+        # Recursive call on each half
+        mergeSort(left)
+        mergeSort(right)
+
+        # Two iterators for traversing the two halves
+        i = j = k = 0
+        while i < len(left) and j < len(right):
+            if left[i] <= right[j]:
+              # The value from the left half has been used
+              myList[k] = left[i]
+              # Move the iterator forward
+              i += 1
+            else:
+                myList[k] = right[j]
+                j += 1
+            # Move to the next slot
+            k += 1
+
+        # For all the remaining values
+        while i < len(left):
+            myList[k] = left[i]
+            i += 1
+            k += 1
+
+        while j < len(right):
+            myList[k]=right[j]
+            j += 1
+            k += 1
+
+str_input= input( )
+myList = [int(x) for x in str_input.split()] 
+
+mergeSort(myList)
+print(myList)
